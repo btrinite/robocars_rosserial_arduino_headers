bb849bcddb7d304b01ca04acc6fe3baa1100e758
Benoit TRINITE <btrinite@free.fr>
2020-04-02 16:11:19 +0200

    fix

--- update_rosserial_arduino_headers.sh
+++ update_rosserial_arduino_headers.sh
@@ -3,8 +3,8 @@ rm -r -f ./ros_lib
 rm -f ./ros_lib.tgz
 rosrun rosserial_arduino make_libraries.py .
 sleep 2
-cp ros_lib_fix/ArduinoHardware.h ../ros_lib
-cp ros_lib_fix/ros.h ../ros_lib
+cp ros_lib_fix/ArduinoHardware.h ros_lib
+cp ros_lib_fix/ros.h ros_lib
 git add -A ros_lib
 git commit -m "Update"
 git push origin master
